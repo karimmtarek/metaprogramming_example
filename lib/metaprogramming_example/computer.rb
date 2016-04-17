89ef4084e177875516720614d1c057be9e0bf0ee
Karim Tarek <karimmtarek@gmail.com>
2016-04-17 19:25:51 -0400

    Complete start point
    - Add data sample
    - Add specs for computer class
    - Improve data_source class

---
+++ lib/metaprogramming_example/computer.rb
@@ -0,0 +1,44 @@
+class Computer
+  attr_reader :id, :data_source
+
+  def initialize(computer_id, data_source)
+    @id = computer_id
+    @data_source = data_source
+  end
+
+  def mouse
+    info = data_source.get_mouse_info(id)
+    price = data_source.get_mouse_price(id)
+    "Mouse: ​#{info}​ ($​#{price}​)"
+  end
+
+  def cpu
+    info = data_source.get_cpu_info(id)
+    price = data_source.get_cpu_price(id)
+    "Cpu: ​#{info}​ ($​#{price}​)"
+  end
+
+  def keyboard
+    info = data_source.get_keyboard_info(id)
+    price = data_source.get_keyboard_price(id)
+    "Keyboard: ​#{info}​ ($​#{price}​)"
+  end
+
+  def memory
+    info = data_source.get_memory_info(id)
+    price = data_source.get_memory_price(id)
+    "Memory: ​#{info}​ ($​#{price}​)"
+  end
+
+  def motherboard
+    info = data_source.get_motherboard_info(id)
+    price = data_source.get_motherboard_price(id)
+    "Motherboard: ​#{info}​ ($​#{price}​)"
+  end
+
+  def storage
+    info = data_source.get_storage_info(id)
+    price = data_source.get_storage_price(id)
+    "Storage: ​#{info}​ ($​#{price}​)"
+  end
+end
