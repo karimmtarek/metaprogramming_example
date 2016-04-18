fbeb8d6b514f4bcb0661436083e528a7615fc24f
Karim Tarek <karimmtarek@gmail.com>
2016-04-18 12:40:49 -0400

    Refactor: dynamic method

--- lib/metaprogramming_example/computer.rb
+++ lib/metaprogramming_example/computer.rb
@@ -6,35 +6,18 @@ class Computer
     @data_source = data_source
   end
 
-  def mouse
-    component :mouse
+  def self.define_component(name)
+    define_method(name) do
+      info = data_source.send("get_#{name}_info", id)
+      price = data_source.send("get_#{name}_price", id)
+      "#{name.capitalize}: ​#{info}​ ($​#{price}​)"
+    end
   end
 
-  def cpu
-    component :cpu
-  end
-
-  def keyboard
-    component :keyboard
-  end
-
-  def memory
-    component :memory
-  end
-
-  def motherboard
-    component :motherboard
-  end
-
-  def storage
-    component :storage
-  end
-
-  private
-
-  def component(name)
-    info = data_source.send("get_#{name}_info", id)
-    price = data_source.send("get_#{name}_price", id)
-    "#{name.capitalize}: ​#{info}​ ($​#{price}​)"
-  end
+  define_component :mouse
+  define_component :cpu
+  define_component :keyboard
+  define_component :memory
+  define_component :motherboard
+  define_component :storage
 end
